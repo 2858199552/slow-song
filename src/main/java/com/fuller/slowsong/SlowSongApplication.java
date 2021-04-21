140a197d5e69f2377b6fba91261dc024815b9f3c
zhongqi <2858199552@qq.com>
2021-04-21 11:17:26 +0800

    first commit

---
+++ src/main/java/com/fuller/slowsong/SlowSongApplication.java
@@ -0,0 +1,13 @@
+package com.fuller.slowsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowSongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowSongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fuller/slowsong/SlowSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuller.slowsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowSongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
